0042304219313d82a09cdcbb4e488509144a2964
Bossdoli18 <oladeleife18@gmail.com>
2023-04-25 13:47:27 -0700

    modified alto

--- Alto.c
+++ Alto.c
@@ -4,11 +4,22 @@
 #include "sys/wait.h"
 #include "unistd.h"
 
+/*
+ * This is a simple command prompt written in C.
+ * It reads user input and executes the specified command,
+ * displaying the output on the console.
+ * The program runs in an infinite loop
+ * The prompt is cleared after each command is executed, 
+ * and the user can exit the program by entering the "exit" command.
+ */
+
+// Function to read user input
 void read_command(char cmd[], char *par[]) {
     char line[1024];
     int count = 0, i = 0, j = 0;
     char *array[100], *pch;
 
+// Read input character by character until a newline is encountered
     for (;;) {
         int c = fgetc(stdin);
         if (c == EOF) {
@@ -47,6 +58,8 @@ void read_command(char cmd[], char *par[]) {
     }
 }
 
+// Function to print the prompt
+
 void type_prompt() {
     static int first_time = 1;
     if (first_time) {
@@ -58,17 +71,22 @@ void type_prompt() {
     printf("#");
 }
 
+/*main function
+ *Runs an infinite loop to keep the command prompt running
+ *Fork a child process to execute the command
+ *Exit the program if the user enters the "exit" command
+ */
 int main() {
     char cmd[100], command[100], *parameters[20];
 
-    char *envp[] = { (char *) "PATH=C:\\Windows\\System32", 0 };
+    char *envp[] = { (char *) "PATH=C:", 0 };
     while(1) {
         type_prompt();
         read_command(command, parameters);
         if (fork() != 0) {
             wait(NULL);
         } else {
-            strcpy(cmd, "C:\\Windows\\System32");
+            strcpy(cmd, "C:");
             strcpy(cmd, command);
             int result = execvp(cmd, parameters);
             if (result == -1) {
