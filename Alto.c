5c961b24188d7807ea5b5bf62c5b5d3fc326bb59
Chi-Naydu <johnvianneychinedu@gmail.com>
2023-04-27 19:28:51 -0700

    fixing Alto.c betty still

--- Alto.c
+++ Alto.c
@@ -29,51 +29,54 @@ char *array[100], *pch;
 for (;;)
 {
 	int c = fgetc(stdin);
-	if(c == EOF)
+	if (c == EOF)
 	{
 		fprintf(stderr, "Error: Could not read from stdin\n");
 		exit(1);
 	}
 	line[count++] = (char) c;
-	if(c == '\n') {
+	if (c == '\n')
+	{
 		break;
 	}
+	
 	if (count == 1)
 	{
 		return;
         }
-        pch = strtok(line, "\n");
-        if (pch == NULL) {
-        fprintf(stderr, "Error: Could not tokenize input\n");
-        exit(1);
+	pch = strtok(line, "\n");
+	if (pch == NULL)
+	{
+		fprintf(stderr, "Error: Could not tokenize input\n");
+		exit(1);
 	}
-
-        while (pch != NULL)
+	
+	while (pch != NULL)
 	{
 		array[i++] = strdup(pch);
-		if (array[i-1] == NULL)
+		if (array[i - 1] == NULL)
 		{
 			fprintf(stderr, "Error: Could not allocate memory for token\n");
 			exit(1);
 		}
 		pch = strtok(NULL, "\n");
 	}
-       
 	strcpy(cmd, array[0]);
-	
-	for (int j = 0; j < i; j++) {
+	for (int j = 0; j < i; j++)
+	{
 		par[j] = array[j];
 		par[i] = NULL;
 	}
 }
 }
-
-// Function to print the prompt
-
-void type_prompt()
+/*
+ *Function to print the prompt
+ */
+void type_prompt(void)
 {
 	static int first_time = 1;
-	if(first_time) {
+	if (first_time)
+	{
 		const char* CLEAR_SCREEN_ANSI = "\e[1;1H\e[2J";
 		write(STDERR_FILENO, CLEAR_SCREEN_ANSI, 12);
 		first_time = 0;
@@ -81,34 +84,39 @@ void type_prompt()
 	printf("#");
 }
 
-/*main function
+/*
+ *main function
  *Runs an infinite loop to keep the command prompt running
  *Fork a child process to execute the command
  *Exit the program if the user enters the "exit" command
  */
-int main() {
-	
+int main(void)
+{
 	char cmd[100], command[100], *parameters[20];
-	
 	char *envp[] = { (char *) "PATH=C:", 0 };
-	while(1) {
+	while (1)
+	{
 		type_prompt();
 		read_command(command, parameters);
-		if (fork() != 0) {
+		if (fork() != 0)
+		{
 			wait(NULL);
-        } else {
-		strcpy(cmd, "C:");
-		strcpy(cmd, command);
-		int result = execvp(cmd, parameters);
-		if (result == -1) {
+		} else
+		{
+			strcpy(cmd, "C:");
+			strcpy(cmd, command);
+			int result = execvp(cmd, parameters);
+		if (result == -1)
+		{
 			printf("Error: Command not found.\n");
 			exit(1);
 		}
 	}
-		if (strcmp(command, "exit") == 0) {
+		if (strcmp(command, "exit") == 0)
+		{
 			break;
 		}
 		type_prompt();
 	}
-	return 0;
+	return (0);
 }
